f0c6c32a11312a6f951a55b9d2e78021fb7701aa
Kaushik-Shahare <kaushikshahare0@gmail.com>
2024-07-11 09:29:24 +0530

    Added new page Home

--- src/main/java/com/example/Home.java
+++ src/main/java/com/example/Home.java
@@ -7,11 +7,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/hello")
-public class HelloServlet extends HttpServlet {
+@WebServlet("")
+public class Home extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html");
-        response.getWriter().println("<h1 style='color: red;'>Hello, World!</h1>");
+        response.getWriter().println("<h1 style='color: blue;'>Dashboard</h1>");
     }
 }
